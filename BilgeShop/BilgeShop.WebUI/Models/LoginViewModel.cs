36f48d8bfee87332bc419a186085983aea1a2b52
sertanbozkus <sertan.bozkus@bilgeadam.com>
2023-01-28 05:08:31 +0300

    Add files via upload

---
+++ BilgeShop/BilgeShop.WebUI/Models/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BilgeShop.WebUI.Models
+{
+    public class LoginViewModel
+    {
+       
+        public string Email { get; set; }
+        
+        public string Password { get; set; }
+
+    }
+}
